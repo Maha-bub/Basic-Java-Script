db0e9e7f144a356439a418854774d41c7e3d883c
Mahabu-bub <mdmahabubulalam0511@gmail.com>
2026-02-19 11:18:01 +0600

    pracitce switch

---
+++ Common/Basic-Java-Script/conditional.js
@@ -0,0 +1,122 @@
+// // // var num = 12;
+// // // if (num > 10) {
+// // //     console.log("conditon is right");
+
+// // // }
+// // // else{
+// // //     console.log("condition is false");
+// // // }
+
+
+// // // num1=15;
+// // // if(num1%2 == 0)
+// // // {
+// // //     console.log("the number is even")
+
+// // // }else{
+// // //     console.log("the number is odd")
+// // // }
+
+
+// // let salary = 25000;
+// // let hasCar = true;
+// // let Bcs = false;
+
+// // // if(salary>=26000 && hasCar == true){
+// // //     console.log("rich")
+// // // }else{
+// // //     console.log("poor")
+// // // }
+
+// // // if(salary>=28000 || hasCar==true && Bcs == true){
+// // //     console.log("good canditate")
+// // // } else{
+// // //     console.log("not good maching")
+// // // }
+
+// // let result = (salary>=25000 && hasCar == true) && (Bcs == true) ?  " good candidate":"not my standard";
+// // console.log(result);
+
+// // let salary = 25000;
+// // let hasCar = true;
+// // let Bcs = false;
+
+// // if(salary >=30000){
+// //     console.log("Good Salary");
+// // } else if(hasCar == true || Bcs== true){
+// //     console.log("standard package");
+// // }
+// // else{
+// //     console.log("gorib");
+// // }
+
+// var mark =29;
+// if (mark >= 80) {
+//     console.log("A+");
+// } else if (mark >= 70) {
+//     console.log("B+");
+
+// }
+// else if (mark >= 60) {
+//     console.log("B");
+// } else if (mark >= 50) {
+//     console.log("C");
+// } else if (
+//     mark >= 40
+// ) {
+//     console.log("C+");
+// }
+// else if (mark >= 33) {
+//     console.log(D);
+// }
+// else {
+//     console.log("F");
+// };
+//  let a=23, b=32, c=17;
+//  if(a>b && a>c){
+//     console.log("Largest number is: a" )
+//  } else if(b>c && b>a){
+//     console.log("largest number is: b" )
+   
+//  }
+ 
+//  else {
+//     console.log("largest number is c" );
+//  }
+
+let charecter="A"
+switch (charecter){
+    case "A":
+        console.log("Vowel");
+        break;
+    case "E":
+        console.log("Vowel");
+        break;
+    case "I":
+        console.log("Vowel");
+        break;
+    case "O":
+        console.log("Vowel");
+        break;
+    case "U":
+        console.log("Vowel")
+        break;
+    case "a":
+        console.log("Vowel");
+        break;
+    case "e":
+        console.log("Vowel");
+        break;
+    case "i":
+        console.log("Vowel");
+        break;
+    case "o":
+        console.log("Vowel");
+        break;
+    case "u":
+        console.log("Vowel")
+        break;
+    default:
+        console.log("Consonant");
+}
+
---
+++ Common/Basic-Java-Script/script.js
@@ -0,0 +1,45 @@
+// // let x=8;
+// // console.log(x===8);
+
+// // let y="b";
+// // console.log(y==="b");
+
+// // const pi=3.1416;
+// // document.writeln(pi===3.1416);
+
+// // var num=16;
+// // document.writeln(num !=6);
+
+// // var continues= parseFloat(3.1416);
+
+// // var intigers=parseInt("34");
+// // // document.writeln(intigers);
+// // console.log(typeof intigers);
+// // // console.log(continues);
+// // console.log(typeof continues);
+
+// // a=true;
+// // b=false;
+// // c=false;
+// // console.log(a&&b&& c);
+
+// // console.log(a|| b || c);
+
+// //  a=5, b=3, c=7;
+
+
+// //  Arry in js and array methods intro
+
+
+// var Student_info=["Anita","Sumi","Meem","Antor","Bappi","Mahabub","Joy","Mahim"]
+// document.writeln(Student_info);
+
+
+// Student_info.pop();
+// console.log(Student_info);
+// Student_info.shift("Anita");
+
+// console.log(Student_info);
+
+const array_items = ["Alam", "Bappi" , "Turna" , "Anita" , "Sinthiya" , "Mim" ,"Madhuri","Sumi"];
+console.log(array_items);
