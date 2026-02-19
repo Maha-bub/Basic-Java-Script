490e684488b521137b053e287680ca071ac97024
Mahabu-bub <mdmahabubulalam0511@gmail.com>
2026-02-19 11:28:39 +0600

    find largest number from 3 number

---
+++ Common/Basic-Java-Script/find_lagest.js
@@ -0,0 +1,11 @@
+ let a=23, b=32, c=17;
+ if(a>b && a>c){
+    console.log("Largest number is: a" )
+ } else if(b>c && b>a){
+    console.log("largest number is: b" )
+   
+ }
+ 
+ else {
+    console.log("largest number is c" );
+ }
