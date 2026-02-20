125a867259440598d3b16a69b422c05111695875
Your Name <you@example.com>
2026-02-20 17:31:47 +0600

    push an elements of a array

--- Common/Basic-Java-Script/array.js
+++ Common/Basic-Java-Script/array.js
@@ -5,3 +5,10 @@ console.log(individual);
  fruits[2] = "jambura";
  console.log(fruits)
   console.log(fruits[5]);
+
+  
+  var location =["dhanmondi lake","hatirjhil","Robindro srobor"]
+  console.log(location);
+
+  location.push("aftabnagar");
+  console.log(location);
