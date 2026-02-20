0443a6f9064715d4a1c364faead8730babb5af13
Your Name <you@example.com>
2026-02-20 17:26:03 +0600

    update one line code

---
+++ Common/Basic-Java-Script/array.js
@@ -0,0 +1,7 @@
+const fruits = ["mango", "Banana","Apple","Orange","Pineapple","watermelon"]
+console.log(fruits);
+var individual= fruits[2];
+console.log(individual);
+ fruits[2] = "jambura";
+ console.log(fruits)
+  console.log(fruits[5]);
---
+++ Common/Basic-Java-Script/push-pop.js
@@ -0,0 +1,6 @@
+const ages=[12,45,457,57,45,87,21,42,]
+ages.push(4555);
+ages.push(14,52,12,35);
+console.log(ages)
+ages.pop()
+console.log(ages)
