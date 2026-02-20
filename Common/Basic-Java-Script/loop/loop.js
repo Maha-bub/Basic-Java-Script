524b83e7fcaba281ec139f43bb417cd93d097e15
Your Name <you@example.com>
2026-02-21 00:12:38 +0600

    practice sorting function

--- Common/Basic-Java-Script/loop/loop.js
+++ Common/Basic-Java-Script/loop/loop.js
@@ -2,5 +2,11 @@ const  numbers = [12, 98, 45, 56, 894, 847, 98, 78, 67, 53]
 console.log(numbers);
 
 for (const num of numbers){
-        console.log(num);
+              console.log(num);
     }
+
+
+    let number = [14,76,98,8,57,34];
+    var sorted = number.sort(x,y);
+    console.log(sorted);
+    
