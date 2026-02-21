0ce58692faecd4923230f7ea3ff069018de0d51a
Your Name <you@example.com>
2026-02-21 21:47:39 +0600

    multiplication of 5 table

--- Common/Basic-Java-Script/loop/while.js
+++ Common/Basic-Java-Script/loop/while.js
@@ -1,22 +1,35 @@
-let i=1;
-while(i<=60){
-    console.log("I will invest at least 6 hours every single day for next 60 days")
+// let i=1;
+// while(i<=60){
+//     console.log("I will invest at least 6 hours every single day for next 60 days")
+//     i++;
+// }
+
+
+// let num = 61;
+// while(num<=100){
+//     if(num%2 === 1){
+//         console.log("This is a odd number:",num)
+
+//     }
+//     num++;
+// }
+// let num1 = 78;
+// while(num1<=100){
+//     if(num1%2 ===0){
+//         console.log("This is even number:", num1);
+//     }
+//     num1++;
+// }
+
+
+let num3 = 5;
+let i = 1;
+while (i<=10){
+    console.log(num3 + "*" + i + "=" + (num3*i));
     i++;
+
 }
 
-
-let num = 61;
-while(num<=100){
-    if(num%2 === 1){
-        console.log("This is a odd number:",num)
-
-    }
-    num++;
-}
-let num1 = 78;
-while(num1<=100){
-    if(num1%2 ===0){
-        console.log("This is even number:", num1);
-    }
-    num1++;
-}
+for(i =1; i<=10; i++){
+   console.log(num3 + "*"+i + "=" + (num3*i));
+};
