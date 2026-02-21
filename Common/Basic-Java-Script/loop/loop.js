a60e23a44b2f5e3519b676b19e3456a9aa1b1382
Your Name <you@example.com>
2026-02-21 15:08:31 +0600

    while loop concept practice

--- Common/Basic-Java-Script/loop/loop.js
+++ Common/Basic-Java-Script/loop/loop.js
@@ -1,12 +1,26 @@
-const  numbers = [12, 98, 45, 56, 894, 847, 98, 78, 67, 53]
-console.log(numbers);
+// const  numbers = [12, 98, 45, 56, 894, 847, 98, 78, 67, 53]
+// console.log(numbers);
 
-for (const num of numbers){
-              console.log(num);
-    }
+// numbers.sort((a , b)=> b-a);
+// console.log(numbers)
 
 
-    let number = [14,76,98,8,57,34];
-    var sorted = number.sort(x,y);
-    console.log(sorted);
+//     let number = [14,76,98,8,57,34];
+//     var sorted = number.sort((x,y) => y-x);
+//     console.log(sorted);
     
+
+var num = 0 ;
+// if (num<=30){
+//     console.log("This number is so big")
+// } else{
+//     console.log("this is a number but so small")
+// }
+
+
+// while loop usu___________
+while(num<5){
+    console.log("looping continuesly", );
+    num = num+1;
+}
+
