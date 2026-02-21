96eb6cdb4ffb25909a168c0cc6072dadd80f94b1
Your Name <you@example.com>
2026-02-21 15:12:23 +0600

    odds numbers from 61 to 100

--- Common/Basic-Java-Script/loop/while.js
+++ Common/Basic-Java-Script/loop/while.js
@@ -3,3 +3,13 @@ while(i<=60){
     console.log("I will invest at least 6 hours every single day for next 60 days")
     i++;
 }
+
+
+let num = 61;
+while(num<=100){
+    if(num%2 === 1){
+        console.log("This is a odd number:",num)
+
+    }
+    num++;
+}
