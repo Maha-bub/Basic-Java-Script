27f0e6b88401590d6f1aa2335d80458ad4835fc4
Mahabu-bub <mdmahabubulalam0511@gmail.com>
2026-02-22 10:11:32 +0600

    grade count using switch statement

---
+++ Common/Basic-Java-Script/s-grade.js
@@ -0,0 +1,23 @@
+let mark=45;
+switch(true){
+    case (mark>=33 && mark <=39):
+        console.log("D");
+        break;
+    case (mark>=40 && mark <=49):
+        console.log("C-");
+        break;
+    case (mark>=53 && mark <=59):
+        console.log("C");
+        break;
+    case (mark>=60 && mark <=69):
+        console.log("B+");
+        break;
+    case (mark>=70 && mark <=79):
+        console.log("B");
+        break;
+    case (mark>=80 && mark <=100):
+        console.log("A+");
+        break;
+    default:
+        console.log("F"); 
+}
