46ca14c1af490013a2fe373271edee4b927d56e7
Mahabu-bub <mdmahabubulalam0511@gmail.com>
2026-02-22 12:09:33 +0600

    use forEach loop

--- Common/Basic-Java-Script/for_loop.js
+++ Common/Basic-Java-Script/for_loop.js
@@ -8,17 +8,25 @@ for(i; i<students.length; i++){
 
 
 
-// for in loop
-let info={
-    name: "Mahabub", 
-    Age: 20,
-    model:"y30"
-}   //key is a name of variable
-for(key in info){
-    console.log(key + ":" + info[key])
-}
+// // for in loop
+// let info={
+//     name: "Mahabub", 
+//     Age: 20,
+//     model:"y30"
+// // }   //key is a name of variable
+// // for(key in info){
+// //     console.log(key + ":" + info[key])
+// }
+
+
+// javascrip forEach loop
 
 
     // ++i means first assign then value of i update
     // i-- means first value of i is update and then assign
 
+const fruits =[ "apple", "orange"];
+fruits.forEach(info);
+function info(item){
+    console.log(item)
+}
