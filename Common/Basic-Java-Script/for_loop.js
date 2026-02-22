2645c77ae28e62d099c5853c5a70bca71c733948
Mahabu-bub <mdmahabubulalam0511@gmail.com>
2026-02-22 11:24:08 +0600

    array lenght and for in loop

---
+++ Common/Basic-Java-Script/for_loop.js
@@ -0,0 +1,24 @@
+let students =["shakil","Anita","sumi","Mim","Layes","Mahabub"]
+
+let i=0;
+for(i; i<students.length; i++){
+    console.log(students[i])
+ }
+
+
+
+
+// for in loop
+let info={
+    name: "Mahabub", 
+    Age: 20,
+    model:"y30"
+}   //key is a name of variable
+for(key in info){
+    console.log(key + ":" + info[key])
+}
+
+
+    // ++i means first assign then value of i update
+    // i-- means first value of i is update and then assign
+
