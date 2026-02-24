b5054bfc4b818f593f9ac2ae79ef73b242c62532
Mahabu-bub <mdmahabubulalam0511@gmail.com>
2026-02-24 11:00:42 +0600

    arrow-function

---
+++ Common/Basic-Java-Script/Function/string-revers.js
@@ -0,0 +1,7 @@
+let sentence ="I am a professional webdeveloper."
+for( let letters of sentence){
+    let revers=''
+    // console.log(letters)
+     revers = letters + revers 
+     console.log(revers);
+}
