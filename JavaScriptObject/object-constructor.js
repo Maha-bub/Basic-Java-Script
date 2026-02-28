cfbab83ab68e762eb0e101ac491d2849916cfaa0
Mahabu-bub <mdmahabubulalam0511@gmail.com>
2026-02-28 10:34:14 +0600

    object-constructor

---
+++ JavaScriptObject/object-constructor.js
@@ -0,0 +1,7 @@
+function person(FirstName, Name, fullname){
+    this.FirstName= FirstName;
+    this.LastName = Name;
+    this.FullName = fullname;
+}
+let Identity = new person("Mahabub"+" " + "Alam","Mahabub Alam");
+console.log(Identity.FirstName + " " +Identity.LastName);
